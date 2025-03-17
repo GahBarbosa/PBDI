a1d85a8787daa20a9024550653e9e090aefd215b
Gabriel Barbosa <gahbarbosa7@gmail.com>
2025-03-17 17:01:19 -0300

    subtrair

--- exer_01/menu.py
+++ exer_01/menu.py
@@ -1,10 +1,14 @@
 def somar(a, b):
   return a + b
 
+def subtrair(a, b):
+  return a - b
+
 def menu():
   while True:
     print("\nEscolha uma opção:")
     print("1. Somar")
+    print("2. Subtrair")
     print("0. Sair")
       
     opcao = input("Opção: ")
@@ -20,6 +24,8 @@ def menu():
         
         if opcao == "1":
           print(f"Resultado: {somar(a, b)}")
+        elif opcao == "2":
+          print(f"Resultado: {subtrair(a, b)}")
           
       except ValueError:
         print("Entrada inválida. Certifique-se de digitar números válidos.")
