f1da7a81d570e1fec2d5123a4d1ace62db4d6648
Gabriel Barbosa <gahbarbosa7@gmail.com>
2025-04-24 09:45:26 -0300

    ap 16: 1.2 - cursor vinculado

--- ap_16/ap_16.sql
+++ ap_16/ap_16.sql
@@ -45,3 +45,28 @@ BEGIN
 
     CLOSE ref;
 END $$;
+
+-- 1.2 Escreva um cursor que exibe todos os nomes dos youtubers em ordem reversa
+
+-- resposta com cursor vinculado
+DO $$
+DECLARE
+    cur_reverse CURSOR FOR
+        SELECT youtuber
+        FROM  tb_top_youtubers
+        ORDER BY youtuber ASC;
+    
+    v_youtuber TEXT;
+BEGIN
+    OPEN cur_reverse;
+
+    FETCH LAST FROM cur_reverse INTO v_youtuber;
+
+    WHILE FOUND LOOP
+        RAISE NOTICE 'Youtuber: %', v_youtuber;
+
+        FETCH PRIOR FROM cur_reverse INTO v_youtuber;
+    END LOOP;
+
+    CLOSE cur_reverse;
+END $$;
