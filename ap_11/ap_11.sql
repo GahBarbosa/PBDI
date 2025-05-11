dc41d7aa81a64c0ff89d34a83e72d70de0b0bce3
Gabriel Barbosa <gahbarbosa7@gmail.com>
2025-05-11 05:08:58 -0300

    ap 11: 1.3

--- ap_11/ap_11.sql
+++ ap_11/ap_11.sql
@@ -193,22 +193,55 @@ BEGIN
 END;
 $$;
 
+-- 1.2 Adicione um procedimento ao sistema do restaurante. Ele deve:
+-- receber um parâmetro de entrada (IN) que representa o código de um cliente 
+-- exibir, com RAISE NOTICE, o total de pedidos que o cliente tem 
 CREATE OR REPLACE PROCEDURE sp_total_pedidos_cliente (
-    IN p_cod_cliente INT
+  IN p_cod_cliente INT
 )
 LANGUAGE plpgsql
 AS $$
 DECLARE
-    total_pedidos INT;
+  total_pedidos INT;
 BEGIN
-    SELECT COUNT(*)
-    INTO total_pedidos
+  SELECT COUNT(*)
+  INTO total_pedidos
+  FROM tb_pedido
+  WHERE cod_cliente = p_cod_cliente;
+
+  RAISE NOTICE 'O cliente % possui % pedido(s).', p_cod_cliente, total_pedidos;
+
+  INSERT INTO tb_log (data_execucao, nome_procedure)
+  VALUES (CURRENT_TIMESTAMP, 'sp_total_pedidos_cliente');
+END;
+$$;
+
+
+-- 1.3 Reescreva o exercício 1.2 de modo que o total de pedidos seja armazenado em uma variável de saída (OUT). 
+DROP PROCEDURE IF EXISTS sp_total_pedidos_cliente(INTEGER);
+
+CREATE OR REPLACE PROCEDURE sp_total_pedidos_cliente (
+    IN p_cod_cliente INT,
+    OUT total_pedidos INT
+)
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    SELECT COUNT(*) INTO total_pedidos
     FROM tb_pedido
     WHERE cod_cliente = p_cod_cliente;
 
-    RAISE NOTICE 'O cliente % possui % pedido(s).', p_cod_cliente, total_pedidos;
-
-    INSERT INTO tb_log (data_execucao, nome_procedure)
+    INSERT INTO tb_log (data_execucao, procedimento)
     VALUES (CURRENT_TIMESTAMP, 'sp_total_pedidos_cliente');
+END;
+$$;
+
+
+DO $$
+DECLARE
+    total_pedidos INT;
+BEGIN
+    CALL sp_total_pedidos_cliente(1, total_pedidos);
+    RAISE NOTICE 'Total: % pedido(s)', total_pedidos;
 END;
 $$;
